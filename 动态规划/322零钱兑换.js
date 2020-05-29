374035c938afea12be487229f57fad49d39b05af
licb <545360100@qq.com>
2020-05-29 11:18:12 +0800

    feat: 🎸 换硬币的非动态规划方法

--- 动态规划/322零钱兑换.js
+++ 动态规划/322零钱兑换.js
@@ -25,4 +25,29 @@ var coinChange = function (coins, amount) {
   return dp[amount] > amount ? -1 : dp[amount];
 };
 
+/** 思路：贪心回溯 + dfs https://leetcode-cn.com/problems/coin-change/solution/322-by-ikaruga/ */
+var change = (coins, amount, coinIndex, count, ans) => {
+  if (amount === 0) { // 符合条件
+    ans[0] = Math.min(ans[0], count);
+    return;
+  }
+  if (coinIndex == coins.length) return;
+  // 计算最大能投几个
+  let t = Math.floor(amount / coins[coinIndex]);
+  // k + count < ans[0]: 如果此时的硬币数就已经大于ans了，那么接下来的比较都是多余的
+  for (let k = t; k >= 0 && k + count < ans[0]; k--) {
+    change(coins, amount - k * coins[coinIndex], coinIndex + 1, count + k, ans);
+  }
+};
+
+var coinChange2 = function (coins, amount) {
+  if (!amount) return 0;
+  coins.sort((a, b) => b - a);
+  let ans = [Number.MAX_SAFE_INTEGER];
+  change(coins, amount, 0, 0, ans);
+  return ans[0] == Number.MAX_SAFE_INTEGER ? -1 : ans[0];
+};
+
 coinChange([2], 3);
+const a = coinChange2([1,2,3,4,5,6,7,8,9,10,12,3131,12312,4234454,6452],42344541244)
+console.log(a)
