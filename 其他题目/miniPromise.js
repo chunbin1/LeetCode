9274305c6f928600492ac58c158e2b2ca52c4c27
licb <545360100@qq.com>
2021-06-24 17:48:34 +0800

    feat: 🎸 链式调用

--- 其他题目/miniPromise.js
+++ 其他题目/miniPromise.js
@@ -52,6 +52,7 @@ class MyPromise {
   };
 
   then = (onFulfilled, onRejected) => {
+    debugger
     // onFulfilled、onRejected 是可选参数。如果 onFulfilled 不是函数，则必须忽略它；如果 onRejected 不是函数，则必须忽略它；
     onFulfilled = typeof onFulfilled === 'function' ? onFulfilled : (value) => value;
     onRejected =
@@ -61,35 +62,88 @@ class MyPromise {
             throw error;
           };
 
-    switch (this.status) {
-      case STATUS.PENDING: {
-        this.onRejectedCallback.push(onRejected);
-        this.onFulfilledCallback.push(onFulfilled);
-        break;
-      }
-      case STATUS.FULFILLED: {
-        onFulfilled(this.value);
-        break;
-      }
-      case STATUS.REJECTED: {
-        onRejected(this.reason);
-        break;
-      }
+    let resolve
+    let reject 
+    // 弄个新的Promise
+    const promise2 = new MyPromise((_resolve, _reject) => {
+      resolve = _resolve
+    });
+
+    if (this.status === STATUS.PENDING) {
+      this.onFulfilledCallback.push(() => {
+        const x = onFulfilled(this.value);
+        resolvePromise(promise2, x, resolve, reject);
+      });
+      this.onRejectedCallback.push(() => {
+        const x = onRejected(this.value);
+        resolvePromise(promise2, x, resolve, reject);
+      });
+    } else if (this.status === STATUS.FULFILLED) {
+      const x = onFulfilled(this.value);
+      resolvePromise(promise2, x, resolve, reject);
+    } else if (this.status === STATUS.REJECTED) {
+      const x = onRejected(this.error);
+      resolvePromise(promise2, x, resolve, reject);
     }
+
+    return promise2;
   };
+}
+
+function resolvePromise(promise2, x, resolve, reject) {
+  debugger
+  // 如果 promise2 === x， 执行 reject，错误原因为 TypeError
+  if (promise2 === x) {
+    reject(new TypeError('The promise and the return value are the same'));
+  }
+
+  // 如果 x 是函数或对象
+  if (typeof x === 'object' || typeof x === 'function') {
+    let then;
+    try {
+      then = x.then;
+    } catch (error) {
+      reject(error);
+    }
+
+    // 如果 x.then 是函数
+    if (typeof then === 'function') {
+      then.call(
+        x,
+        (y) => {
+          // resolve的结果依旧是promise 那就继续解析
+          resolvePromise(promise2, y, resolve, reject);
+        },
+        (err) => {
+          reject(err); // 失败了
+        },
+      );
+    } else {
+      // 如果 x.then 不是函数
+      resolve(x);
+    }
+  } else {
+    // 如果 x 不是 promise 实例
+    resolve(x);
+  }
 }
 
 const mypromise = new MyPromise((resolve, reject) => {
   resolve('成功');
 });
 
-mypromise.then(data => {
-  console.log(data, '1')
-})
+const mypromise2 = new MyPromise((resolve, reject) => {
+  resolve('成功2');
+});
 
-mypromise.then(data => {
-  console.log(data, '2')
-})
+mypromise
+  .then((data) => {
+    console.log(data, '1');
+    return mypromise2
+  }).then(data=>{
+    console.log(data,'2')
+
+  })
 // const promise = new Promise(function(resolve, reject) {
 //   // ... some code
 
